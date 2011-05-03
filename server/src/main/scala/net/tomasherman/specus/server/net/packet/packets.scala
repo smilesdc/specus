c74c7bcdbda1b53b45935ae41afb49ebe57ebdda
Tomas Herman <tomas.herman@gmail.com>
2011-05-04 01:40:37 +0200

    Added licence

--- server/src/main/scala/net/tomasherman/specus/server/net/packet/packets.scala
+++ server/src/main/scala/net/tomasherman/specus/server/net/packet/packets.scala
@@ -1,11 +1,23 @@
 package net.tomasherman.specus.server.net.packet
 
 /**
- * Created by IntelliJ IDEA.
- * User: tomasherman
- * Date: 03/05/2011
- * Time: 21:37
- * To change this template use File | Settings | File Templates.
+ * This file is part of Specus.
+ *
+ * Specus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * Author: Tom
  */
+
 
 sealed abstract class Packet(val id:Byte)

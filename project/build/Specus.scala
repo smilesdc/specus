cec9d85bc903ef95562e3f347f1892085e5cf609
Tomas Herman <tomas.herman@gmail.com>
2011-05-03 19:47:24 +0200

    Added Configgy, Netty & Akka dependency.

--- project/build/Specus.scala
+++ project/build/Specus.scala
@@ -2,10 +2,25 @@ import sbt._
 
 class SpecusProject(info:ProjectInfo) extends ParentProject(info) with IdeaProject{
 
+
   lazy val api = project("api","Specus API", new DefaultProject(_) with IdeaProject)
+  lazy val server = project("server","Specus Server", new ServerProject(_),api)
+  lazy val node = project("node","Specus Node",new NodeProject(_) with IdeaProject,api)
 
-  lazy val server = project("server","Specus Server",new DefaultProject(_) with IdeaProject,api)
-  lazy val node = project("node","Specus Node",new DefaultProject(_) with IdeaProject,api)
+  val twitterRepo = "Twitter maven repository" at "http://maven.twttr.com/"
 
+
+  class ServerProject(info:ProjectInfo) extends DefaultProject(info) with IdeaProject with AkkaProject{
+    val jbossRepo = "jBoss repository" at "https://repository.jboss.org/nexus/content/repositories/releases/"
+    val netty = "org.jboss.netty" % "netty" % "3.2.4.Final" //Apache2
+    val configgy = "net.lag" % "configgy" %  "1.6.10" //from twitter repo, Apache2
+    val akkaRemote = akkaModule("remote")
+  }
+
+  class NodeProject(info:ProjectInfo) extends DefaultProject(info) with IdeaProject with AkkaProject{
+    val configgy = "net.lag" % "configgy" %  "1.6.10" //from twitter repo, Apache2
+    val akkaRemote = akkaModule("remote")
+  }
 
 }
+

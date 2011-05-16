6016e61141d264bc82ac08638d231bc5665c6e04
Tomas Herman <tomas.herman@gmail.com>
2011-05-16 20:43:16 +0200

    Added ProtocolEncoder and ProtocolDecoder interfaces

---
+++ api/src/main/scala/net/tomasherman/specus/api/net/ProtocolTraits.scala
@@ -0,0 +1,35 @@
+package net.tomasherman.specus.api.net
+
+import org.jboss.netty.buffer.ChannelBuffer
+import packet.Packet
+
+/**
+ * This file is part of Specus.
+ *
+ * Specus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Specus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Specus.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+trait ProtocolDecoder{
+
+  def decode(buffer:ChannelBuffer):Packet
+
+}
+
+trait ProtocolEncoder{
+
+  def encode(packet:Packet):ChannelBuffer
+
+}

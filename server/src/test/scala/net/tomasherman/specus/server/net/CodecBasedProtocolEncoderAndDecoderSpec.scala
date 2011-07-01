06c120e2a90f7f90ded57c0ae89e05a55d74ddbb
Tomas Herman <tomas.herman@gmail.com>
2011-07-01 14:05:10 +0200

    Added CodecBasedProtocolDecoder tests and merged together with Encoder tests.

--- server/src/test/scala/net/tomasherman/specus/server/net/CodecBasedProtocolEncoderAndDecoderSpec.scala
+++ server/src/test/scala/net/tomasherman/specus/server/net/CodecBasedProtocolEncoderAndDecoderSpec.scala
@@ -4,7 +4,7 @@ import org.specs2.mutable.Specification
 import org.specs2.mock._
 import net.tomasherman.specus.server.api.net.packet.Packet
 import org.jboss.netty.buffer.ChannelBuffers
-import net.tomasherman.specus.server.api.net.{PacketEncoderNotFoundException, Codec, CodecRepository}
+import net.tomasherman.specus.server.api.net.{BufferDecoderNotFoundException, PacketEncoderNotFoundException, Codec, CodecRepository}
 
 /**
  * This file is part of Specus.
@@ -34,17 +34,23 @@ object TestEnv extends Mockito{
     val repMock = mock[CodecRepository]
     val codec1 = mock[Codec[TestPacket1]]
     codec1.encode(new TestPacket1()) returns ChannelBuffers.copiedBuffer(Array[Byte](0x00,0x01))
+    codec1.decode(ChannelBuffers.copiedBuffer(Array[Byte](0x01))) returns new TestPacket1()
     val codec2 = mock[Codec[TestPacket2]]
     codec2.encode(new TestPacket2()) returns ChannelBuffers.copiedBuffer(Array[Byte](0x01,0x02))
+    codec2.decode(ChannelBuffers.copiedBuffer(Array[Byte](0x02))) returns new TestPacket2()
     repMock.lookupCodec(new TestPacket1()) returns Some(codec1)
+    repMock.lookupCodec(0x00.toByte) returns Some(codec1)
     repMock.lookupCodec(new TestPacket2()) returns Some(codec2)
+    repMock.lookupCodec(0x01.toByte) returns Some(codec2)
     repMock.lookupCodec(new TestPacket3()) returns None
+    repMock.lookupCodec(0x03.toByte) returns None
 
     repMock
   }
 }
 
 class Encoder(val env:{val codecRepository:CodecRepository}) extends CodecBasedProtocolEncoder
+class Decoder(val env:{val codecRepository:CodecRepository}) extends CodecBasedProtocolDecoder
 
 class CodecBasedProtocolEncoderSpec extends Specification{
   val encoder = new Encoder(TestEnv)
@@ -56,6 +62,16 @@ class CodecBasedProtocolEncoderSpec extends Specification{
     }
     "should fail properly" in {
       encoder.encode(new TestPacket3()) must throwAn[PacketEncoderNotFoundException]
+    }
+  }
+  "CodecBasedProtocolDecoder" should  {
+  val decoder = new Decoder(TestEnv)
+    "should decode protocol according to the codecs" in {
+      decoder.decode(ChannelBuffers.copiedBuffer(Array[Byte](0x00,0x01))) must_==  new TestPacket1()
+      decoder.decode(ChannelBuffers.copiedBuffer(Array[Byte](0x01,0x02))) must_==  new TestPacket2()
+    }
+    "should fail properly" in {
+      decoder.decode(ChannelBuffers.copiedBuffer(Array[Byte](0x03))) must throwAn[BufferDecoderNotFoundException]
     }
   }
 }

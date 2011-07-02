39b863c5f4e1e165b33a5e9fd3d3d9f64527ce8e
Tomas Herman <tomas.herman@gmail.com>
2011-07-02 18:50:03 +0200

    Added SpecusEncoder

---
+++ server/src/main/scala/net/tomasherman/specus/server/net/SpecusEncoder.scala
@@ -0,0 +1,30 @@
+package net.tomasherman.specus.server.net
+
+import org.jboss.netty.handler.codec.oneone.OneToOneEncoder
+import org.jboss.netty.channel.{Channel, ChannelHandlerContext}
+import net.tomasherman.specus.server.api.net.packet.Packet
+import net.tomasherman.specus.server.api.net.CodecRepository
+
+/**
+ * This file is part of Specus.
+ *
+ * Specus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Specus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Specus.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+class SpecusEncoder(val env:{val codecRepository:CodecRepository}) extends OneToOneEncoder with CodecBasedProtocolEncoder{
+  def encode(ctx: ChannelHandlerContext, channel: Channel, msg: AnyRef) = {
+    encode(msg.asInstanceOf[Packet])
+  }
+}

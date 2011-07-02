796d714505e6fe97fa7c0d81fb0b1b8091b7685d
Tomas Herman <tomas.herman@gmail.com>
2011-07-02 19:26:58 +0200

    Untested EncodingCodec implemented

---
+++ server/src/main/scala/net/tomasherman/specus/server/net/EncodingCodec.scala
@@ -0,0 +1,35 @@
+package net.tomasherman.specus.server.net
+
+import net.tomasherman.specus.server.api.net.Codec
+import net.tomasherman.specus.server.api.net.packet.Packet
+import org.jboss.netty.buffer.ChannelBuffer
+/**
+ * This file is part of Specus.
+ *
+ * Specus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Specus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Specus.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+abstract class EncodingCodec[T<:Packet](packetId:Byte,packetClass:Class[T]) extends Codec(packetId,packetClass){
+
+  def encode(packet:T) = {
+    val buffer = createChannelBuffer
+    buffer.writeByte(packetId)
+    encodeDataToBuffer(packet,buffer)
+    buffer
+  }
+
+  protected def createChannelBuffer:ChannelBuffer
+  protected def encodeDataToBuffer(packet:T,buffer:ChannelBuffer):Unit
+}

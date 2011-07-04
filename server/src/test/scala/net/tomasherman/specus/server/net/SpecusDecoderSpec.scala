ac58c6a46ef6e4be1fb549c85b497422edfb9b9c
Tomas Herman <tomas.herman@gmail.com>
2011-07-05 00:37:05 +0200

    SpecusDecoder tests implemented.

--- server/src/test/scala/net/tomasherman/specus/server/net/SpecusDecoderSpec.scala
+++ server/src/test/scala/net/tomasherman/specus/server/net/SpecusDecoderSpec.scala
@@ -3,12 +3,12 @@ package net.tomasherman.specus.server.net
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
 import org.specs2.mock.Mockito
-import net.tomasherman.specus.server.api.net.{Codec, CodecRepository}
 import net.tomasherman.specus.server.api.net.packet.Packet
 import net.tomasherman.specus.server.api.net.DecodingUtils._
 import net.tomasherman.specus.server.api.net.EncodingUtils._
 import org.jboss.netty.buffer.{ChannelBuffer, ChannelBuffers}
-import org.jboss.netty.handler.codec.embedder.DecoderEmbedder
+import net.tomasherman.specus.server.api.net.{BufferDecoderNotFoundException, Codec, CodecRepository}
+import org.jboss.netty.handler.codec.embedder.{CodecEmbedderException, DecoderEmbedder}
 
 /**
  * This file is part of Specus.
@@ -32,7 +32,7 @@ import org.jboss.netty.handler.codec.embedder.DecoderEmbedder
 class Env(val codecRepository:CodecRepository)
 case class DSTestPacket1(p1:Int,p2:String,p3:Byte) extends Packet
 class TestDecodingCodec1 extends Codec[DSTestPacket1](0x01,classOf[DSTestPacket1]){
-  def encode(packet: DSTestPacket1) = null //irelevant
+  def encode(packet: DSTestPacket1) = null //irrelevant
 
   def decode(buffer: ChannelBuffer) = {
     val p1 = decodeInt(buffer)
@@ -51,6 +51,15 @@ trait SpecusDecoderTestScope extends Scope with Mockito {
   encodeByte(0x00,validBuffer1Bytes)
   val validBuffer1 = ChannelBuffers.copiedBuffer(validBuffer1Bytes)
   crMock lookupCodec(0x01.toByte) returns Some(new TestDecodingCodec1)
+
+  val invalidBufferBytes = ChannelBuffers.dynamicBuffer()
+  encodeByte(0x01,invalidBufferBytes)
+  encodeInt(1337,invalidBufferBytes)
+
+  val noCodecBufferBytes = ChannelBuffers.dynamicBuffer()
+  val failPacketId = 0x66:Byte
+  encodeByte(failPacketId,noCodecBufferBytes)
+  crMock.lookupCodec(0x66:Byte) returns None  //add None as result of 0x66 lookupCodec in repository
 }
 
 class SpecusDecoderSpec extends Specification{
@@ -59,9 +68,31 @@ class SpecusDecoderSpec extends Specification{
     "decode valid buffers properly" in new SpecusDecoderTestScope{
       val decoder = new SpecusDecoder(env)
       val emb = new DecoderEmbedder[Packet](decoder)
-      emb.offer(ChannelBuffers.copiedBuffer(validBuffer1Bytes))
+      emb.offer(validBuffer1Bytes)
       emb.poll() must_== new DSTestPacket1(1337,":-)",0)
     }
+
+    "decode null if buffer isn't valid" in new SpecusDecoderTestScope {
+      val decoder = new SpecusDecoder(env)
+      val emb = new DecoderEmbedder[Packet](decoder)
+      emb.offer(invalidBufferBytes)
+      emb.poll must_== null
+    }
+
+    "fail if no codec found" in new SpecusDecoderTestScope {
+      val decoder = new SpecusDecoder(env)
+      val emb = new DecoderEmbedder[Packet](decoder)
+      try {
+        emb.offer(noCodecBufferBytes)
+      } catch {
+        case e:CodecEmbedderException => e.getCause match {
+          case cause:BufferDecoderNotFoundException => cause.packetId must_== failPacketId
+          case _ => failure
+        }
+        case _ => failure
+      }
+    }
   }
+
 
 }

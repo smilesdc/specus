faab3f47160665f405462d06f2fcc32cc165498d
Tomas Herman <tomas.herman@gmail.com>
2011-07-20 19:25:57 +0200

    MajorMinorBuildPluginVersion with tests added.

---
+++ server/src/main/scala/net/tomasherman/specus/server/plugin/MajorMinorBuildPluginVersion.scala
@@ -0,0 +1,65 @@
+package net.tomasherman.specus.server.plugin
+
+import net.tomasherman.specus.server.api.plugin.PluginVersion
+import net.tomasherman.specus.server.api.logging.Logging
+
+/**
+ * This file is part of Specus.
+ *
+ * Specus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Specus is distributed in the hope other it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Specus.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+class MajorMinorBuildPluginVersion(val major: Option[Int], val minor: Option[Int], val build: Option[Int])
+  extends PluginVersion with Logging {
+
+  def this(major: Int) = this(Some(major),None,None)
+  def this(major: Int,minor: Int) = this(Some(major),Some(minor),None)
+  def this(major: Int,minor: Int,build: Int) = this(Some(major),Some(minor),Some(build))
+
+  def compare(other: PluginVersion) = {
+    other match {
+      case x:MajorMinorBuildPluginVersion => Some(compare(x))
+      case _ => None
+    }
+  }
+  
+  private def compare(other: MajorMinorBuildPluginVersion) = {
+    var res = compareLevel(major,other.major)
+    if(res == 0) {
+      res = compareLevel(minor,other.minor)
+      if(res == 0) {
+        res = compareLevel(build,other.build)
+      }
+    }
+    res
+  }
+
+  private def compareLevel(me: Option[Int],other:Option[Int]) = {
+    (me,other) match {
+      case (None,None) => 0
+      case (Some(x),Some(y)) => { x compare y }
+      case _ => 0
+    }
+  }
+
+  def canCompare(other: PluginVersion) = {
+    other match {
+      case x:MajorMinorBuildPluginVersion => true
+      case _ => false
+    }
+  }
+}
---
+++ server/src/test/scala/net/tomasherman/specus/server/plugin/MajorMinorBuildPluginVersionSpec.scala
@@ -0,0 +1,69 @@
+package net.tomasherman.specus.server.plugin
+
+import org.specs2.mutable.Specification
+import org.specs2.mock.Mockito
+import org.specs2.matcher.ThrownExpectations
+import net.tomasherman.specus.server.api.plugin.PluginVersion
+
+/**
+ * This file is part of Specus.
+ *
+ * Specus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Specus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Specus.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+class MajorMinorBuildPluginVersionSpec extends Specification with Mockito with ThrownExpectations{
+  type mmb = MajorMinorBuildPluginVersion
+  "MajorMinorBuildPluginVersion" should {
+    "canCompare properly" in {
+      val v1 = new mmb(1,3,3)
+      val v2 = mock[PluginVersion]
+      val v3 = new mmb(1,3,3)
+
+      v1.canCompare(v2) must_== false
+      v1.canCompare(v3) must_== true
+      v3.canCompare(v1) must_== true
+      v2.canCompare(v1) must_== false
+      v2.canCompare(v3) must_== false
+    }
+    "compare properly" in {
+      val v1 = new mmb(1,1,1)
+      val v2 = new mmb(1,1,2)
+      val v3 = new mmb(1,2,1)
+      val v4 = new mmb(1,1)
+      val v5 = new mmb(1,2)
+      val v6 = new mmb(2,1)
+      val v7 = new mmb(1)
+      val v8 = new mmb(2)
+
+      v1.compare(v2) must_== Some(-1)
+      v1.compare(v1) must_== Some(0)
+      v1.compare(v3) must_== Some(-1)
+      v1.compare(v4) must_== Some(0)
+      v1.compare(v5) must_== Some(-1)
+      v1.compare(v6) must_== Some(-1)
+      v1.compare(v7) must_== Some(0)
+      v1.compare(v8) must_== Some(-1)
+
+      v6.compare(v8) must_== Some(0)
+      v6.compare(v7) must_== Some(1)
+      v6.compare(v5) must_== Some(1)
+      v6.compare(v6) must_== Some(0)
+
+      v3.compare(v6) must_== Some(-1)
+      v3.compare(v5) must_== Some(0)
+    }
+  }
+
+}

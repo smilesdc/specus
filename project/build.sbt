33bc09665e0e173e5e770c9ab0e8c8dce25b07e1
Tomas Herman <tomas.herman@gmail.com>
2011-07-27 03:05:58 +0200

    Got rid of build.sbt and plugins

--- project/build.sbt
+++
@@ -1,4 +0,0 @@
-name := "Specus"
-version := "0.0.0-dev"
-organization := "net.tomasherman"
-scalaVersion := "2.9.0"
--- project/plugins/project/Build.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-object Plugins extends Build {
-  lazy val root = Project("root", file(".")) dependsOn(
-	  uri("git://github.com/eed3si9n/sbt-assembly.git"),
-	  uri("git://github.com/ijuma/sbt-idea.git#sbt-0.10")
-    )
-}
